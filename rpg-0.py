d7a0485bab2c7d32233fef6bb7194070726b5820
Dustin Myers <dustinblaine.myers@gmail.com>
2020-01-21 16:43:06 -0500

    added zombie class for bonus question and put attack function inside character class as method.

--- rpg-0.py
+++ rpg-0.py
@@ -1,5 +1,6 @@
 # Hero class.
 class Character():
+
     def __init__(self, name, health, power):
         self.power = power
         self.health = health
@@ -9,25 +10,32 @@ class Character():
         if (self.health <= 0):
             return False
         else: return True
+    def attack(self, defender):
+        if isinstance(defender, zombie):
+            print('you are here.')
+            pass
+        else:
+            defender.health = defender.health - self.power
+            return defender.health
 
     def printHealth(self):
         print("The health of the {} is {} and the power is {}".format(self.name, self.health, self.power))
     
-    def choice(self):
+    def choice(self, other ):
         print("You have {} health and {} power.".format(hero.health, hero.power))
-        print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
+        print("The {} has {} health and {} power.".format(other.name, other.health, other.power))
         print()
         print("What do you want to do?")
-        print("1. fight goblin")
+        print("1. fight {}".format(other.name))
         print("2. do nothing")
         print("3. flee")
         print("> ",)
         user_input = input()
         if user_input == "1":
-            attack(hero, goblin)
-            print("You do {} damage to the goblin.".format(self.power))
-            if goblin.health <= 0:
-                print("The goblin is dead.")
+            self.attack(other)
+            print("You do {} damage to the {}.".format(self.power , other.name ))
+            if other.health <= 0:
+                print("The {} is dead.".format(other.name))
         elif user_input == "2":
             return 2
         elif user_input == "3":
@@ -36,9 +44,7 @@ class Character():
         else:
             print("Invalid input %r" % user_input)
 
-def attack(attacker, defender):
-    defender.health = defender.health - attacker.power
-    return defender.health
+
 
 
 class hero(Character):
@@ -51,6 +57,15 @@ class hero(Character):
         self.health = health
         self.name = name
 
+class zombie(Character):
+    power = 2
+
+    
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+
 # goblin class
 class goblin(Character):
     health = 6
@@ -62,14 +77,16 @@ class goblin(Character):
         self.name = name
 
 
-def game():
-    while (hero.isAlive() and goblin.isAlive()):
-        hero.choice()
-       
-        attack(goblin,hero)
+def game(hero, enemy):
+    while (hero.isAlive() and enemy.isAlive()):
+        hero.choice(enemy)
+        print("you made it past choice.")
+        enemy.attack(hero)
+
+
 
         
-    
+zard = zombie("zardilla",1,2)    
 goblin = goblin("goblin",6, 2)
 hero = hero("hero", 10, 5)
-game()
+game(hero, zard)
