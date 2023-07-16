bb3b7dc9686b8d8873e9d3c301335113af6bda15
Dominik <65331556+lordlogo2002@users.noreply.github.com>
2023-07-16 11:31:36 +0200

    Update rpg-0.py
    Made the code more readable

--- rpg-0.py
+++ rpg-0.py
@@ -6,29 +6,65 @@ In this simple RPG game, the hero fights the goblin. He has the options to:
 3. flee
 
 """
+class Entity:
+    def __init__(self, name:str, health:int, power:int):
+        self.name = name
+        self.health = health
+        self.power = power
+        self.alive = self.health > 0
+
+    def take_damage(self, other):
+        """
+        take damage from another Entity and update alive flag
+
+        Args:
+            other (Entity): the attacking Entity from which this will get damaged
+        """
+        self.health -= other.power
+        if self.health <= 0:
+            print(f"{self.name} got hit by {other.name} with a deadly attack")
+            self.alive = False
+            return
+        print(f"{self.name} got attacked by {other.name} losing {other.power} hp")
+
+    def attack(self, other):
+        """
+        attack other Entity by using the `take_damage`method
+
+        Args:
+            other (Entity): the other entity to deal damage on
+        """
+        other.take_damage(self)
+
+    @property
+    def stats(self) -> tuple:
+        return self.health, self.power
+
+def ask_for_action(hero:Entity, goblin:Entity) -> str:
+    """
+    Show the stats of the hero and goblin and ask for a
+    action. no validation of input
+
+    Returns:
+        str: user action
+    """
+    print("You have %d health and %d power." % hero.stats)
+    print("The goblin has %d health and %d power." % goblin.stats)
+    print()
+    print("What do you want to do?")
+    print("1. fight goblin")
+    print("2. do nothing")
+    print("3. flee")
+    return input("> ")
 
 def main():
-    hero_health = 10
-    hero_power = 5
-    goblin_health = 6
-    goblin_power = 2
+    hero = Entity("Hero", 10, 5)
+    goblin = Entity("Goblin", 6, 2)
 
-    while goblin_health > 0 and hero_health > 0:
-        print("You have %d health and %d power." % (hero_health, hero_power))
-        print("The goblin has %d health and %d power." % (goblin_health, goblin_power))
-        print()
-        print("What do you want to do?")
-        print("1. fight goblin")
-        print("2. do nothing")
-        print("3. flee")
-        print("> ",)
-        user_input = input()
+    while goblin.alive and hero.alive:
+        user_input = ask_for_action(hero, goblin)
         if user_input == "1":
-            # Hero attacks goblin
-            goblin_health -= hero_power
-            print("You do %d damage to the goblin." % hero_power)
-            if goblin_health <= 0:
-                print("The goblin is dead.")
+            hero.attack(goblin)
         elif user_input == "2":
             pass
         elif user_input == "3":
@@ -37,11 +73,8 @@ def main():
         else:
             print("Invalid input %r" % user_input)
 
-        if goblin_health > 0:
-            # Goblin attacks hero
-            hero_health -= goblin_power
-            print("The goblin does %d damage to you." % goblin_power)
-            if hero_health <= 0:
-                print("You are dead.")
+        if goblin.alive:
+            goblin.attack(hero)
 
-main()
+if __name__ == "__main__":
+    main()
